4f1e0f6fdf09ea8f2dbf3e1dd5f8ba9032024c1e
Kyle Wood <kyle@denwav.dev>
2021-06-13 20:37:57 -0500

    Remove changes I didn't mean to push

--- paperweight-core/src/main/kotlin/PaperweightCore.kt
+++ paperweight-core/src/main/kotlin/PaperweightCore.kt
@@ -43,7 +43,7 @@ import org.gradle.kotlin.dsl.*
 
 class PaperweightCore : Plugin<Project> {
     override fun apply(target: Project) {
-        val extension = target.extensions.create(Constants.EXTENSION, PaperweightCoreExtension::class)
+        target.extensions.create(Constants.EXTENSION, PaperweightCoreExtension::class)
 
         target.gradle.sharedServices.registerIfAbsent("download", DownloadService::class) {}
 
@@ -78,24 +78,6 @@ class PaperweightCore : Plugin<Project> {
         }
 
         target.afterEvaluate {
-            target.repositories {
-                maven(extension.paramMappingsRepo) {
-                    content {
-                        onlyForConfigurations(Constants.PARAM_MAPPINGS_CONFIG)
-                    }
-                }
-                maven(extension.remapRepo) {
-                    content {
-                        onlyForConfigurations(Constants.REMAPPER_CONFIG)
-                    }
-                }
-                maven(extension.decompileRepo) {
-                    content {
-                        onlyForConfigurations(Constants.DECOMPILER_CONFIG)
-                    }
-                }
-            }
-
             // Setup the server jar
             val cache = target.layout.cache
 
