aba3695bb036f07fd642acfa6ec4a159ca0f6a5e
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-07-21 17:04:37 -0700

    Ensure patchCraftBukkit runs when CraftBukkit has changed

--- paperweight-lib/src/main/kotlin/util/file.kt
+++ paperweight-lib/src/main/kotlin/util/file.kt
@@ -54,6 +54,7 @@ fun FileSystemLocationProperty<*>.pathProvider(path: Provider<Path?>) = fileProv
 
 fun DirectoryProperty.convention(project: Project, path: Provider<Path?>) = convention(project.layout.dir(path.map { it.toFile() }))
 fun RegularFileProperty.convention(project: Project, path: Provider<Path?>) = convention(project.layout.file(path.map { it.toFile() }))
+fun DirectoryProperty.convention(project: Project, path: Path) = convention(project.layout.dir(project.provider { path.toFile() }))
 
 val Path.isLibraryJar: Boolean
     get() = name.endsWith(".jar") && !name.endsWith("-sources.jar")
