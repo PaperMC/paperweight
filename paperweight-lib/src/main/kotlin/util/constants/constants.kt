59c469af45e09b2302a3de353f60cfd309609024
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-07-27 20:59:52 -0700

    Remove unneeded constant

--- paperweight-lib/src/main/kotlin/util/constants/constants.kt
+++ paperweight-lib/src/main/kotlin/util/constants/constants.kt
@@ -75,7 +75,6 @@ const val SERVER_LIBRARIES = "$DATA_PATH/ServerLibraries.txt"
 private const val TASK_CACHE = "$PAPER_PATH/taskCache"
 
 const val FINAL_REMAPPED_JAR = "$TASK_CACHE/minecraft.jar"
-const val FINAL_DECOMPILE_JAR = "$TASK_CACHE/decompileJar.jar"
 
 fun Task.paperTaskOutput(ext: String) = paperTaskOutput(name, ext)
 fun paperTaskOutput(name: String, ext: String) = "$TASK_CACHE/$name.$ext"
