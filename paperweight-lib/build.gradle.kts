a1b1b140a2e85cd9c8d0d0a5bc08944b0c4a72b6
Jason <11360596+jpenilla@users.noreply.github.com>
2021-10-24 00:14:34 -0700

    Update Hypo to 1.2.2 (#102)

--- paperweight-lib/build.gradle.kts
+++ paperweight-lib/build.gradle.kts
@@ -10,6 +10,9 @@ dependencies {
     implementation(libs.bundles.asm)
 
     implementation(libs.bundles.hypo)
+    implementation(libs.slf4j.log4j2.impl) {
+        isTransitive = false // we get slf4j-api from hypo, gradle provides log4j
+    }
     implementation(libs.bundles.cadix)
 
     implementation(libs.lorenzTiny)
