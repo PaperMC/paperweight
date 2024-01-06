b512c9a882062545b618f035e66d3544b78e653d
Jake Potrebic <jake.m.potrebic@gmail.com>
2024-01-06 13:37:58 -0800

    more work on history stuff

--- paperweight-lib/src/main/kotlin/io/papermc/paperweight/tasks/mm/ApplyApiPatches.kt
+++ paperweight-lib/src/main/kotlin/io/papermc/paperweight/tasks/mm/ApplyApiPatches.kt
@@ -3,7 +3,10 @@ package io.papermc.paperweight.tasks.mm
 import io.papermc.paperweight.tasks.*
 import io.papermc.paperweight.util.*
 import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.provider.ListProperty
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 
@@ -15,8 +18,13 @@ abstract class ApplyApiPatches : BaseTask() {
     @get:InputDirectory
     abstract val patchesDir: DirectoryProperty
 
+    @get:Optional
+    @get:Input
+    abstract val unneededFiles: ListProperty<String>
+
     @get:OutputDirectory
     abstract val outputDir: DirectoryProperty
+
 
     override fun init() {
         group = "mm"
@@ -28,6 +36,11 @@ abstract class ApplyApiPatches : BaseTask() {
         Git.checkForGit()
 
         Git(bukkitDir).let { git ->
+            if (unneededFiles.isPresent && unneededFiles.get().size > 0) {
+                unneededFiles.get().forEach { path -> outputDir.path.resolve(path).deleteRecursive() }
+                git(*Git.add(false, ".")).executeSilently()
+                git("commit", "-m", "Initial", "--author=Initial Source <auto@mated.null>").executeSilently()
+            }
             patchesDir.asFileTree.files.sorted().forEach { patch ->
                 git("am", patch.absolutePath).execute()
             }
