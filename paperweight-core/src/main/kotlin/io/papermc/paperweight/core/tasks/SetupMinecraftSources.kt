977c36a617b73430e5ad3c8ad51838c2a314806a
Bjarne Koll <git@lynxplay.dev>
2025-03-14 20:02:45 +0100

    Revert config to previous value

--- paperweight-core/src/main/kotlin/io/papermc/paperweight/core/tasks/SetupMinecraftSources.kt
+++ paperweight-core/src/main/kotlin/io/papermc/paperweight/core/tasks/SetupMinecraftSources.kt
@@ -217,14 +217,26 @@ abstract class SetupMinecraftSources : JavaLauncherTask() {
         }
 
         val upstream = Git.open(rootProjectDir.toFile())
+        val upstreamConfig = upstream.repository.config
+        val upstreamReachableSHA1 = upstreamConfig.getString("uploadpack", null, "allowreachablesha1inwant")
+        val upstreamConfigContainsUploadPack = upstreamConfig.sections.contains("uploadpack")
         try {
             // Temporarily allow fetching reachable sha1 refs from the "upstream" paper repository.
-            upstream.repository.config.setBoolean("uploadpack", null, "allowreachablesha1inwant", true);
-            upstream.repository.config.save()
+            upstreamConfig.setBoolean("uploadpack", null, "allowreachablesha1inwant", true)
+            upstreamConfig.save()
             oldPaperGit.fetch().setDepth(1).setRemote("origin").setRefSpecs(oldPaperCommit.get()).call()
             oldPaperGit.reset().setMode(ResetCommand.ResetType.HARD).setRef(oldPaperCommit.get()).call()
         } finally {
-            upstream.repository.config.unset("uploadpack", null, "allowreachablesha1inwant");
+            if (upstreamReachableSHA1 == null) {
+                if (upstreamConfigContainsUploadPack) {
+                    upstreamConfig.unset("uploadpack", null, "allowreachablesha1inwant")
+                } else {
+                    upstreamConfig.unsetSection("uploadpack", null)
+                }
+            } else {
+                upstreamConfig.setString("uploadpack", null, "allowreachablesha1inwant", upstreamReachableSHA1)
+            }
+            upstreamConfig.save()
             upstream.close()
         }
 
