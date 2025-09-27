9e75415fbb4f99185bcf2aeb6ec20d66726afd99
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-09-27 14:54:52 -0700

    Update snapshot deployment config

--- buildSrc/src/main/kotlin/config-publish.gradle.kts
+++ buildSrc/src/main/kotlin/config-publish.gradle.kts
@@ -95,7 +95,7 @@ val shadowJar by tasks.existing(ShadowJar::class) {
 
 publishing {
     repositories {
-        maven("https://repo.papermc.io/repository/maven-snapshots/") {
+        maven("https://artifactory.papermc.io/artifactory/snapshots/") {
             credentials(PasswordCredentials::class)
             name = "paper"
         }
