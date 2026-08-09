d6bdb284b36151e537dba9a4a2b88af83b171bcc
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2026-08-08 17:59:24 -0700

    perf: cache library source index

--- paperweight-core/src/main/kotlin/io/papermc/paperweight/core/tasks/ImportLibraryFiles.kt
+++ paperweight-core/src/main/kotlin/io/papermc/paperweight/core/tasks/ImportLibraryFiles.kt
@@ -39,6 +39,7 @@ import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.FileCollection
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.logging.LogLevel
+import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Optional
@@ -51,7 +52,7 @@ import org.gradle.work.DisableCachingByDefault
 
 private data class LibraryImport(val libraryFileName: String, val importFilePath: String)
 
-@DisableCachingByDefault(because = "Indexing library sources is faster than loading the result from cache")
+@CacheableTask
 abstract class IndexLibraryFiles : BaseTask() {
 
     @get:InputFiles
@@ -70,6 +71,7 @@ abstract class IndexLibraryFiles : BaseTask() {
     fun run() {
         val possible = ioDispatcher("IndexLibraryFiles").use { dispatcher ->
             findPossibleLibraryImports(libraries.sourcesJars(), dispatcher)
+                .sortedWith(compareBy(LibraryImport::libraryFileName, LibraryImport::importFilePath))
                 .groupBy { it.libraryFileName }
                 .mapValues {
                     it.value.map { v -> v.importFilePath }
