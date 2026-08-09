e5cc2be5449108832644705ea73b4761c43f9b26
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2026-08-08 18:45:15 -0700

    style: order task annotation imports

--- paperweight-core/src/main/kotlin/io/papermc/paperweight/checkstyle/tasks/PaperCheckstyleTask.kt
+++ paperweight-core/src/main/kotlin/io/papermc/paperweight/checkstyle/tasks/PaperCheckstyleTask.kt
@@ -31,9 +31,9 @@ import org.gradle.api.internal.file.FileOperations
 import org.gradle.api.plugins.quality.Checkstyle
 import org.gradle.api.provider.Property
 import org.gradle.api.provider.SetProperty
+import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
-import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Nested
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.PathSensitive
